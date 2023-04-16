8b966fa79d843cd7be88d240e16d35b2059483d9
leoApple123 <130937034+leoApple123@users.noreply.github.com>
2023-04-16 16:23:32 -0600

    Add files via upload

---
+++ Proyecto_Prueba/Proyecto_Prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proyecto_Prueba
+//
+//  Created by Leonardo  on 12/04/23.
+//  Copyright © 2023 Leonardo . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
